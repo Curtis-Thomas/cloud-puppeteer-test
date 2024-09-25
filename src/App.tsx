5b3edde5a6f8dc03e576cb2ac45fccebf3b3673e
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2024-09-25 10:18:40 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Puppeteer Test - cloud computing - Curtis Thomas</h1>
+    </>
+  );
+}
+
+export default App;
